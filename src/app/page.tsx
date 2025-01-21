f65d90b07e0ec31709536702bdcd74db059ddab5
Wallace027Dev <wallaceofc@hotmail.com>
2025-01-21 20:15:49 -0300

    estilizando pagina principal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,70 @@
 "use client";
 import Image from "next/image";
-import { Anchor, Footer, Header, Main } from "./style";
+import { AboutSection, Anchor, AppContainer, Footer, Header, Main } from "./style";
 import mava from "@/_images/mava-logo.webp";
 import motocycle from "@/_images/motocicleta.webp";
 import instagram from "@/_images/instagram.png";
+import hero from "@/_images/hero.webp";
 
 export default function Page() {
+  const linkMava =
+    "https://wa.me/557331911055?text=Vim%20atrav%C3%A9s%20do%20site%20www.mava.org.br%2C%20gostaria%20de%20saber%20mais%20e%20*simular%20um%20plano%20de%20prote%C3%A7%C3%A3o*%20para%20minha%20moto%2C%20Obrigado.";
+
   return (
-    <>
+    <AppContainer>
       <Header>
-        <Image src={mava} alt="Logomarca Mava" />
-        <Anchor href="/signup">Registrar-se</Anchor>
+        <div>
+          <Image src={mava} alt="Logomarca Mava" />
+          <Anchor href="/signup">Registrar-se</Anchor>
+        </div>
       </Header>
       <Main>
-        <h1>
-          <span>Proteja sua moto</span> com a segurança que ela merece!
-        </h1>
-        <p>
-          Adquira agora o nossa proteção completa com benefícios que so a Mava
-          tem para a sua motocicleta.
-        </p>
-        <Anchor href="/signup">Registrar-se</Anchor>
+        <section>
+          <h1>
+            <span>Proteja sua moto</span> com a segurança que ela merece!
+          </h1>
+          <p>
+            Adquira agora a nossa proteção completa com benefícios que só a Mava
+            tem para a sua motocicleta.
+          </p>
+          <Anchor href="/signup">Registrar-se</Anchor>
+        </section>
+        <AboutSection>
+          <Image src={hero} alt="Ilustração da seção sobre a Mava Motos" />
+          <div>
+            <p>
+              Por que escolher a <span>Mava Motos?</span>
+            </p>
+            <h2>Garantia de segurança, assistência e economia.</h2>
+
+            <p>
+              A nossa proteção veicular garante segurança e tranquilidade para
+              proprietários de motos contra furto, roubo, incêndio, acidentes e
+              danos variados. Oferecemos assistência 24 horas para resolver
+              rapidamente situações desagradáveis no trânsito, sem prejudicar
+              sua vida pessoal e profissional.
+            </p>
+
+            <a href={linkMava} target="_blank" rel="noopener noreferrer">
+              Fale com a Mava
+            </a>
+          </div>
+        </AboutSection>
       </Main>
       <Footer>
-        <Image src={motocycle} alt="Motocicleta na mudança de seção" />
+        <Image src={motocycle} alt="Motocicleta ilustrando a seção final" />
         <div>
-          <h2>Aqui na MAVA você tem benefícios de verdade para sua moto!</h2>
-          <h3>Siga nossas redes</h3>
-          <a href="https://www.instagram.com/mavaclube/" target="_blank">
-            <Image src={instagram} alt="Instagram icon" />
+          <h3>Aqui na MAVA você tem benefícios de verdade para sua moto!</h3>
+          <h4>Siga nossas redes</h4>
+          <a
+            href="https://www.instagram.com/mavaclube/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image src={instagram} alt="Ícone do Instagram" />
           </a>
         </div>
       </Footer>
-    </>
+    </AppContainer>
   );
 }
--- src/app/style.ts
+++ src/app/style.ts
@@ -1,17 +1,33 @@
 import styled from "styled-components";
 
+export const AppContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`;
+
 export const Header = styled.header`
   background-color: var(--primary);
-  display: flex;
-  justify-content: center;
-  align-items: center;
   width: 100%;
   height: 80px;
-  gap: 700px;
+  padding: 0 2rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
-  img {
-    width: 160px;
-    object-fit: contain;
+  div {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    max-width: 1220px;
+    width: 100%;
+    margin: 0 auto;
+
+    img {
+      height: 40px;
+      width: 160px;
+      object-fit: contain;
+    }
   }
 `;
 
@@ -27,16 +43,19 @@ export const Anchor = styled.a`
   &:hover {
     background-color: var(--font);
     color: var(--bg-color);
-    transition: .5s;
+    transition: background-color 0.5s ease, color 0.5s ease;
   }
 `;
 
 export const Main = styled.main`
-  max-width: 720px;
-  margin: 84px auto;
+  flex: 1; // Permite que o Main ocupe o espaço restante
+  max-width: 1220px;
+  margin: 84px auto 0;
+  padding: 0 1rem;
   text-align: start;
+
   h1 {
-    font-size: 3rem;
+    font-size: 4rem;
     color: var(--secondary);
 
     span {
@@ -47,28 +66,66 @@ export const Main = styled.main`
   p {
     margin-bottom: 1rem;
     font-weight: 600;
-    font-size: 1.2rem;
+    font-size: 1.5rem;
+  }
+`;
+
+export const AboutSection = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: center;
+  max-width: 1120px;
+  margin-top: 10rem;
+
+  img {
+    width: 100%;
+    max-width: 430px;
+    height: auto;
+    object-fit: contain;
+  }
+
+  div {
+    max-width: 580px;
+    color: var(--primary);
+
+    span {
+      font-weight: 700;
+    }
+
+    a {
+      color: var(--bg-color);
+      background-color: var(--primary);
+      border: var(--secondary) solid 2.5px;
+      border-radius: 2rem;
+      font-weight: 600;
+      padding: 0.5rem 4rem;
+
+      &:hover {
+        color: var(--secondary);
+        transition: background-color 0.5s ease, color 0.5s ease;
+      }
+    }
   }
 `;
 
 export const Footer = styled.footer`
   position: fixed;
   bottom: 0;
+  width: 100%;
+  color: var(--bg-color);
   display: flex;
   justify-content: space-evenly;
   align-items: center;
   height: 420px;
-  width: 100vw;
-  color: var(--bg-color);
-  background: rgb(17, 29, 102);
+  margin-top: auto;
   background: radial-gradient(
     circle,
     rgba(17, 29, 102, 1) 0%,
-    rgba(17, 29, 102, 1) 35%,
     rgba(0, 20, 75, 1) 100%
   );
 
-  h2 {
+  h3 {
     font-size: 2rem;
     font-weight: 700;
     max-width: 400px;
@@ -89,10 +146,11 @@ export const Footer = styled.footer`
       border-radius: 50%;
       width: 32px;
       height: 32px;
-      transition: background-color 0.3s ease;
+      transition: background-color 0.3s ease, box-shadow 0.3s ease;
 
       &:hover {
         background-color: var(--secondary);
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
       }
 
       img {
